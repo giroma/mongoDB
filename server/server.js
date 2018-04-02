4ee131ad6963e78a2e4c6ed19581ee7fd5d0bb7b
Argir Argirov <dj.giro@gmail.com>
2018-04-02 16:32:40 -0400

    mongoose rest api

--- server/server.js
+++ server/server.js
@@ -1,3 +1,26 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
 const {mongoose} = require('./db/mongoose');
 const {Todo} = require('./models/todo');
 const {User} = require('./models/user');
+
+let app = express()
+
+app.use(bodyParser.json())
+
+app.post('/todos', (req, res) => {
+  const todo = new Todo({
+    text: req.body.text
+  })
+
+  todo.save().then((doc) => {
+    res.status(400).send(doc)
+  }, (err) => {
+    res.send(err)
+  })
+})
+
+app.listen(3000, () => {
+  console.log('started on port 3000');
+})
