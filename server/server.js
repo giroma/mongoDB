4feff78d8f327f0cbb492cc30b36b0fe55276cae
Argir Argirov <dj.giro@gmail.com>
2018-04-06 15:25:45 -0400

    heroku commit setup

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ const {User} = require('./models/user');
 const {ObjectID} = require('mongodb');
 
 let app = express()
+const port = process.env.PORT || 3000 //for deployment
 
 app.use(bodyParser.json())
 
@@ -45,8 +46,8 @@ app.get('/todos/:id', (req, res) => {
   })
 })
 
-app.listen(3000, () => {
-  console.log('started on port 3000');
+app.listen(port, () => {
+  console.log(`started on port ${port}`);
 })
 
 module.exports = {app}
